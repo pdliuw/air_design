ee45a39802c8060d49ce00f1ed6c9c19b4df3437
Air <xkrossiapd163@163.com>
2020-09-07 17:52:52 +0800

    add: example project

---
+++ air_design_example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import ai_amap
+import ai_barcode
+import shared_preferences_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AiAmapPlugin.register(with: registry.registrar(forPlugin: "AiAmapPlugin"))
+  AiBarcodePlugin.register(with: registry.registrar(forPlugin: "AiBarcodePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
